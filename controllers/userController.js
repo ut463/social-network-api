a985c0ae4859b09de6f591fb9cf9d7bd499a32a8
ut463 <matthewcomer@comcast.net>
2024-10-15 03:53:00 -0500

    controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,69 @@
 const { User, Thoughts } = require("../models");
 
-module.exports ={
+module.exports = {
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    async getSingleUser(req, res) {
+        try {
+        const user = await User.findOne({ _id: req.params.userId }).select(
+            "-__v"
+        );
     
-}
+        if (!user) {
+            return res.status(404).json({ message: "No user with that ID" });
+        }
+    
+        res.json(user);
+        } catch (err) {
+        res.status(500).json(err);
+        }
+    },
+    async createUser(req, res) {
+        try {
+        const user = await User.create(req.body);
+        res.json(user);
+        } catch (err) {
+        res.status(500).json(err);
+        }
+    },
+    async deleteUser(req, res) {
+        try {
+        const user = await User.findOneAndDelete({ _id: req.params.userId });
+    
+        if (!user) {
+            return res.status(404).json({ message: "No user with that ID" });
+        }
+    
+        await Thoughts.deleteMany({ _id: { $in: user.thoughts } });
+        res.json({ message: "User and associated thoughts deleted!" });
+        } catch (err) {
+        res.status(500).json(err);
+        }
+    },
+    async updateUser(req, res) {
+        try {
+        const user = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        );
+    
+        if (!user) {
+            return res.status(404).json({ message: 'No user with this id!' });
+        }
+    
+        res.json(user);
+        } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+        }
+    },
+    
+    };
--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const User = require('./User');
+const User = require('./user');
 const Thought = require('./Thought');
 
 module.exports = { User, Thought };
